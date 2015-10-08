6d3949952f3c5057914fa2b6c7cae940271ca6e8
sleepbear <sleepbear669@gmail.com>
2015-10-08 23:57:29 +0900

    1

---
+++ src/test/java/FileMonitoringTest.java
@@ -0,0 +1,18 @@
+import daumtrack.oop.filemonitor.FileMonitoring;
+import org.junit.Test;
+
+/**
+ * Created by sleepbear on 2015. 10. 8..
+ */
+public class FileMonitoringTest {
+
+    @Test
+    public void testExistFileMonitoringClass() throws Exception {
+        // Given
+        new FileMonitoring();
+        // When
+
+        // Then
+
+    }
+}
