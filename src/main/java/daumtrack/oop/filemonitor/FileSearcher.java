7ba6c2ca525323998e95b8fa83ffef840589863e
sleepbear <sleepbear669@gmail.com>
2015-10-09 23:42:04 +0900

    search file to path

--- src/main/java/daumtrack/oop/filemonitor/FileSearcher.java
+++ src/main/java/daumtrack/oop/filemonitor/FileSearcher.java
@@ -2,6 +2,8 @@ package daumtrack.oop.filemonitor;
 
 import com.google.common.collect.Lists;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -9,25 +11,29 @@ import java.util.ArrayList;
  */
 public class FileSearcher {
 
-    private ArrayList<FileMetaData> fileMetaDatas = Lists.newArrayList();
+    private ArrayList<FileMetaData> fileMetaDataList = Lists.newArrayList();
 
-    public ArrayList<FileMetaData> search(String path) {
-        return fileMetaDatas;
+    public ArrayList<FileMetaData> search(String path) throws IOException {
+        searchFile(path);
+        return fileMetaDataList;
     }
-//
-//    public void searchFile() throws IOException {
-//        searchDirectory(targetFile, path);
-//    }
-//
-//    private void searchDirectory(File file, String absPath) throws IOException {
-//        File[] files = file.listFiles();
-//        for (File subFile : files) {
-//            if (subFile.isDirectory()) {
-//                searchDirectory(subFile, absPath);
-//            } else {
-//                logger.debug("relative_path : {} {} ", subFile.getName(), subFile.length());
-//            }
-//            fileCount++;
-//        }
-//    }
+
+    public void searchFile(String dirPath) throws IOException {
+        File dir = new File(dirPath);
+        scanDirectory(dir, dirPath);
+    }
+
+    private void scanDirectory(File file, String absPath) throws IOException {
+        File[] files = file.listFiles();
+        for (File childFile : files) {
+            if (childFile.isDirectory()) {
+                scanDirectory(childFile, absPath);
+            } else {
+                String path = childFile.getPath().replace(absPath, "");
+                long length = childFile.length();
+                long modified = childFile.lastModified();
+                fileMetaDataList.add(new FileMetaData(path, length, modified));
+            }
+        }
+    }
 }
--- src/test/java/daumtrack/oop/filemonitor/FileSearcherTest.java
+++ src/test/java/daumtrack/oop/filemonitor/FileSearcherTest.java
@@ -1,11 +1,11 @@
 package daumtrack.oop.filemonitor;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -16,25 +16,56 @@ import static org.junit.Assert.assertNotNull;
  */
 public class FileSearcherTest {
 
-    private String noneFileDirPath;
+    private static String noneFileDirPath;
+    private FileSearcher fileSearcher;
+    private static String testPath;
+    private final int TEST_FILE_NUMBER = 5;
+
+    @BeforeClass
+    static public void start() throws Exception {
+        testPath = "/tmp/test";
+        noneFileDirPath = "/tmp/none";
+
+        File noneDir = new File(noneFileDirPath);
+        File testDir = new File(testPath);
+        noneDir.mkdirs();
+        testDir.mkdir();
+    }
+
+    @AfterClass
+    static public void end() {
+        File noneDir = new File(noneFileDirPath);
+        File testDir = new File(testPath);
+        noneDir.delete();
+        testDir.delete();
+    }
 
     @Before
     public void setUp() throws Exception {
-        noneFileDirPath = "/tmp/gom/none";
-        File file = new File(noneFileDirPath);
-        file.mkdirs();
+        fileSearcher = new FileSearcher();
+
+        for (int i = 0; i < TEST_FILE_NUMBER; i++) {
+            String filePath = String.valueOf(Paths.get(testPath, String.valueOf(UUID.randomUUID())));
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(filePath)));
+            writer.write(String.valueOf(UUID.randomUUID()));
+            writer.close();
+        }
     }
 
     @After
-    public void after() {
-        File file = new File(noneFileDirPath);
-        file.delete();
+    public void tearDown() throws Exception {
+        File file = new File(testPath);
+        for (File file1 : file.listFiles()) {
+            file1.delete();
+        }
     }
 
     @Test
     public void testExistFileSearcher() throws Exception {
         // Given
-        FileSearcher fileSearcher = new FileSearcher();
+        fileSearcher = new FileSearcher();
         // When
 
         // Then
@@ -45,7 +76,7 @@ public class FileSearcherTest {
     @Test
     public void testNoneFileDirSearch() throws Exception {
         // Given
-        FileSearcher fileSearcher = new FileSearcher();
+
         // When
         ArrayList<FileMetaData> search = fileSearcher.search(noneFileDirPath);
         // Then
@@ -53,12 +84,12 @@ public class FileSearcherTest {
     }
 
     @Test
-    public void testName() throws Exception {
+    public void testTestDirSearch() throws Exception {
         // Given
 
         // When
-
+        ArrayList<FileMetaData> search = fileSearcher.search(testPath);
         // Then
-
+        assertThat(search.size(), is(TEST_FILE_NUMBER));
     }
 }
